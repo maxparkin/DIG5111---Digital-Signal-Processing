fe64c4a7bdff47180053f8d19d2c130f34002eea
maxparkin <Max.Parkin@mail.bcu.ac.uk>
2025-02-07 14:54:31 +0000

    Week 2 Tutorial TASK 4 IN PROGRESS

--- Week 2 - Fundamentals Recap (04.02.2025)/Task_4.m
+++ Week 2 - Fundamentals Recap (04.02.2025)/Task_4.m
@@ -7,11 +7,4 @@
 filename = 'piano_middle_C.wav';
 [sig, Fs] = audioread(filename);
 
-audioData = audioinfo(filename);
-
-file_size = Fs * 16 * 7.5533 * 10^-3;
-
-file_size_bytes = file_size / 8;
-
-% The file size is slightly smaller in the calculation as the file header
-% is not accounted for.
+t1 = 
