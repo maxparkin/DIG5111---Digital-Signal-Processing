5118d0f0b919d14d38aee45691922b74d1e86500
maxparkin <Max.Parkin@mail.bcu.ac.uk>
2025-04-08 13:31:25 +0100

    Week 8 Tutorial COMPLETED

---
+++ Week 8 (18.03.2025)/Task 4/Task_4.m
@@ -0,0 +1,8 @@
+% Max Parkin
+% Birmingham City University
+% BSc (Hons) Degree in Sound Engineering and Production with Professional Placement Year
+% Year 2 (2024 – 2025)
+% DIG5111 – Digital Signal Processing
+
+[b, a] = LPF(10000, 44100, 20);
+fvtool(b, a);
